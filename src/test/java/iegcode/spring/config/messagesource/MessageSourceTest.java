941b19ba92f408f8bde4d865c36f6356f7ecd3f2
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-04 21:16:22 +0700

    first commit

---
+++ src/test/java/iegcode/spring/config/messagesource/MessageSourceTest.java
@@ -0,0 +1,50 @@
+package iegcode.spring.config.messagesource;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
+
+public class MessageSourceTest {
+
+    private ApplicationContext applicationContext;
+
+    private MessageSource messageSource;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(TestApplication.class);
+        messageSource = applicationContext.getBean(MessageSource.class);
+    }
+
+    @Test
+    void testDefaultLocale() {
+        String message = messageSource.getMessage("hello", new Object[]{"Gibran"}, Locale.ENGLISH);
+        Assertions.assertEquals("Hello Gibran", message);
+    }
+
+    @Test
+    void testIndonesiaLocal() {
+        String message = messageSource.getMessage("hello", new Object[]{"Gibran"}, new Locale("in_ID"));
+        Assertions.assertEquals("Hello Gibran", message);
+    }
+
+    @SpringBootApplication
+    public static class TestApplication{
+
+        @Bean
+        public MessageSource messageSource(){
+            ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+            messageSource.setBasenames("my");
+            return messageSource;
+        }
+
+    }
+}
