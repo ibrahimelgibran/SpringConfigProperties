b4a2ee8150ab50c2062232ec814d35bc83a4f350
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-07 21:13:46 +0700

    update

---
+++ src/main/java/iegcode/spring/config/runner/ApplicationPropertiesRunner.java
@@ -0,0 +1,26 @@
+package iegcode.spring.config.runner;
+
+import iegcode.spring.config.properties.ApplicationProperties;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class ApplicationPropertiesRunner implements ApplicationRunner {
+
+    @Autowired
+    private ApplicationProperties properties;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info(properties.getName());
+        log.info(String.valueOf(properties.getVersion()));
+        log.info(String.valueOf(properties.isProductionMode()));
+    }
+
+}
